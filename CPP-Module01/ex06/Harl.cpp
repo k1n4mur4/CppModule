0a5099f169788bc51cb3cbf243ba8fe9a165e139
kinamura <i.kosuke0801@gmail.com>
2025-11-15 18:21:30 +0900

    fix: output format

--- CPP-Module01/ex06/Harl.cpp
+++ CPP-Module01/ex06/Harl.cpp
@@ -9,15 +9,18 @@ Harl::~Harl() {
 }
 
 void	Harl::debug(void) {
-	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!" << std::endl;
+	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger." << std::endl;
+	std::cout << "I really do!" << std::endl;
 }
 
 void	Harl::info(void) {
-	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
+	std::cout << "I cannot believe adding extra bacon costs more money." << std::endl;
+	std::cout << "You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
 }
 
 void	Harl::warning(void) {
-	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years, whereas you started working here just last month." << std::endl;
+	std::cout << "I think I deserve to have some extra bacon for free." << std::endl;
+	std::cout << "I’ve been coming for years, whereas you started working here just last month." << std::endl;
 }
 
 void	Harl::error(void) {
@@ -35,12 +38,19 @@ void	Harl::complain(const std::string& level) {
 	}
 	switch(idx) {
 		case 0:
+			std::cout << "[ DEBUG ]" << std::endl;
 			debug();
+			std::cout << std::endl;
 		case 1:
+			std::cout << "[ INFO ]" << std::endl;
 			info();
+			std::cout << std::endl;
 		case 2:
+			std::cout << "[ WARNING ]" << std::endl;
 			warning();
+			std::cout << std::endl;
 		case 3:
+			std::cout << "[ ERROR ]" << std::endl;
 			error();
 			break ;
 		default:
