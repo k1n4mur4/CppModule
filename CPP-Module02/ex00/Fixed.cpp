b3578c7990fd179b93bbdd63f559b3e011d9936d
kinamura <i.kosuke0801@gmail.com>
2025-12-07 02:25:57 +0900

    fix:ex00

--- CPP-Module02/ex00/Fixed.cpp
+++ CPP-Module02/ex00/Fixed.cpp
@@ -4,8 +4,9 @@ Fixed::Fixed() : _fixedPointValue(0) {
 	std::cout << "Default constructor called" << std::endl;
 }
 
-Fixed::Fixed(const Fixed& other) : _fixedPointValue(other.getRawBits()) {
+Fixed::Fixed(const Fixed& other) : _fixedPointValue(0) {
 	std::cout << "Copy constructor called" << std::endl;
+	*this = other;
 }
 
 Fixed& Fixed::operator=(const Fixed& other) {
--- CPP-Module02/ex00/main.cpp
+++ CPP-Module02/ex00/main.cpp
@@ -1,10 +1,11 @@
 #include "Fixed.hpp"
+#include <iostream>
 
-int	main( void ) {
+int main( void ) {
 
-	Fixed	a;
-	Fixed	b( a );
-	Fixed	c;
+	Fixed a;
+	Fixed b( a );
+	Fixed c;
 
 	c = b;
 
